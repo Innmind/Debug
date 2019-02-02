f2f6a2e561a507420bc1a7ba300e221439868bb6
Baptiste Langlade <baptouuuu@gmail.com>
2019-02-02 11:56:41 +0100

    add app graph capturing in cli context

---
+++ src/CLI/CaptureAppGraph.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Debug\CLI;
+
+use Innmind\Debug\Profiler\Section\CaptureAppGraph as Section;
+use Innmind\CLI\{
+    Command,
+    Command\Arguments,
+    Command\Options,
+    Environment,
+};
+
+final class CaptureAppGraph implements Command
+{
+    private $handle;
+    private $section;
+
+    public function __construct(
+        Command $handle,
+        Section $section
+    ) {
+        $this->handle = $handle;
+        $this->section = $section;
+    }
+
+    public function __invoke(Environment $env, Arguments $arguments, Options $options): void
+    {
+        try {
+            ($this->handle)($env, $arguments, $options);
+        } finally {
+            $this->section->capture($this->handle);
+        }
+    }
+
+    public function __toString(): string
+    {
+        return (string) $this->handle;
+    }
+}
