cc2f2b24a028d6d20fe707f3379ddf29ab16bde0
Baptiste Langlade <baptouuuu@gmail.com>
2019-02-02 11:47:47 +0100

    add exception capturing in cli context

---
+++ src/CLI/CaptureException.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Debug\CLI;
+
+use Innmind\Debug\Profiler\Section\CaptureException as Section;
+use Innmind\CLI\{
+    Command,
+    Command\Arguments,
+    Command\Options,
+    Environment,
+};
+
+final class CaptureException implements Command
+{
+    private $handle;
+    private $section;
+
+    public function __construct(
+        Command $handle,
+        Section $section
+    ) {
+        $this->handle = $handle;
+        $this->section = $section;
+    }
+
+    public function __invoke(Environment $env, Arguments $arguments, Options $options): void
+    {
+        try {
+            ($this->handle)($env, $arguments, $options);
+        } catch (\Throwable $e) {
+            $this->section->capture($e);
+
+            throw $e;
+        }
+    }
+
+    public function __toString(): string
+    {
+        return (string) $this->handle;
+    }
+}
---
+++ tests/CLI/CaptureExceptionTest.php
@@ -0,0 +1,111 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\Debug\CLI;
+
+use Innmind\Debug\{
+    CLI\CaptureException,
+    Profiler\Section\CaptureException as Section,
+    Profiler\Profile\Identity,
+};
+use Innmind\CLI\{
+    Command,
+    Command\Arguments,
+    Command\Options,
+    Environment,
+};
+use Innmind\Rest\Client\Server;
+use Innmind\Server\Control\Server\Processes;
+use Innmind\StackTrace\Render;
+use PHPUnit\Framework\TestCase;
+
+class CaptureExceptionTest extends TestCase
+{
+    public function testInterface()
+    {
+        $this->assertInstanceOf(
+            Command::class,
+            new CaptureException(
+                $this->createMock(Command::class),
+                new Section(
+                    $this->createMock(Server::class),
+                    $this->createMock(Processes::class),
+                    new Render
+                )
+            )
+        );
+    }
+
+    public function testStringCast()
+    {
+        $handle = new CaptureException(
+            $inner = $this->createMock(Command::class),
+            new Section(
+                $this->createMock(Server::class),
+                $this->createMock(Processes::class),
+                new Render
+            )
+        );
+        $inner
+            ->expects($this->once())
+            ->method('__toString')
+            ->willReturn('foo');
+
+        $this->assertSame('foo', (string) $handle);
+    }
+
+    public function testDoNothingWhenNoExceptionThrown()
+    {
+        $handle = new CaptureException(
+            $inner = $this->createMock(Command::class),
+            $section = new Section(
+                $server = $this->createMock(Server::class),
+                $this->createMock(Processes::class),
+                new Render
+            )
+        );
+        $env = $this->createMock(Environment::class);
+        $arguments = new Arguments;
+        $options = new Options;
+        $inner
+            ->expects($this->once())
+            ->method('__invoke')
+            ->with($env, $arguments, $options);
+        $server
+            ->expects($this->never())
+            ->method('create');
+
+        $section->start(new Identity('profile-uuid'));
+
+        $this->assertNull($handle($env, $arguments, $options));
+    }
+
+    public function testCaptureThrownException()
+    {
+        $handle = new CaptureException(
+            $inner = $this->createMock(Command::class),
+            $section = new Section(
+                $server = $this->createMock(Server::class),
+                $this->createMock(Processes::class),
+                new Render
+            )
+        );
+        $env = $this->createMock(Environment::class);
+        $arguments = new Arguments;
+        $options = new Options;
+        $inner
+            ->expects($this->once())
+            ->method('__invoke')
+            ->with($env, $arguments, $options)
+            ->will($this->throwException(new \Exception));
+        $server
+            ->expects($this->once())
+            ->method('create');
+
+        $section->start(new Identity('profile-uuid'));
+
+        $this->expectException(\Exception::class);
+
+        $handle($env, $arguments, $options);
+    }
+}
