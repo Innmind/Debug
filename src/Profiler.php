3fea2b3b85679d8e8759a1b1daa3c6f21b207b24
Baptiste Langlade <baptouuuu@gmail.com>
2019-02-09 14:41:57 +0100

    add comments to explain profiler and section api

--- src/Profiler.php
+++ src/Profiler.php
@@ -5,6 +5,12 @@ namespace Innmind\Debug;
 
 use Innmind\Debug\Profiler\Profile\Identity;
 
+/**
+ * Even though multiple profiles can't be done in a single process ending the
+ * profile requires to give the identity as a parameter so only the code that
+ * started the profile can end it, thus preventing any other code to end it
+ * prematurely (at least without some trickery)
+ */
 interface Profiler
 {
     public function start(string $name): Identity;
--- src/Profiler/Section.php
+++ src/Profiler/Section.php
@@ -5,6 +5,10 @@ namespace Innmind\Debug\Profiler;
 
 use Innmind\Debug\Profiler\Profile\Identity;
 
+/**
+ * `start` and `finish` both require the identity to enforce then to be called
+ * only within a profiler so no other code can call them prematurely
+ */
 interface Section
 {
     public function start(Identity $identity): void;
