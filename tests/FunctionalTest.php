b2c4aaabbfb8d47e4428504a22492ba3a09f5abd
Baptiste Langlade <baptouuuu@gmail.com>
2023-05-01 11:41:21 +0200

    add tests

--- tests/FunctionalTest.php
+++ tests/FunctionalTest.php
@@ -20,6 +20,7 @@ use Innmind\Server\Control\Server\Command;
 use Innmind\Http\{
     Message\ServerRequest\ServerRequest,
     Message\Request\Request,
+    Message\Response\Response,
     Message\Method,
     Message\StatusCode,
     ProtocolVersion,
@@ -63,9 +64,8 @@ class FunctionalTest extends TestCase
     public function testRecordHttpContext()
     {
         $app = Application::http(Factory::build(), Environment::test([['PATH', \getenv('PATH')]]))
-            ->map(Debug::inApp()->operatingSystem())
             ->map(Profiler::inApp($this->storage))
-            ->map(Debug::inApp()->app())
+            ->map(Debug::inApp())
             ->appendRoutes(static fn($routes, $_, $os) => $routes->add(
                 Route::literal('GET /')->handle(static function() use ($os) {
                     $os
@@ -151,10 +151,278 @@ class FunctionalTest extends TestCase
         );
     }
 
+    public function testRecordHttpContextWithoutOSLayer()
+    {
+        $app = Application::http(Factory::build(), Environment::test([['PATH', \getenv('PATH')]]))
+            ->map(Profiler::inApp($this->storage))
+            ->map(Debug::inApp()->app())
+            ->appendRoutes(static fn($routes, $_, $os) => $routes->add(
+                Route::literal('GET /')->handle(static function() use ($os) {
+                    $os
+                        ->control()
+                        ->processes()
+                        ->execute(Command::foreground('cat hello'))
+                        ->wait()
+                        ->match(
+                            static fn() => null,
+                            static fn() => null,
+                        );
+                    $os
+                        ->remote()
+                        ->http()(new Request(
+                            Url::of('https://github.com'),
+                            Method::get,
+                            ProtocolVersion::v11,
+                        ))
+                        ->match(
+                            static fn() => null,
+                            static fn() => null,
+                        );
+
+                    throw new \Exception('foo');
+                }),
+            ));
+
+        try {
+            $app->run(new ServerRequest(
+                Url::of('/'),
+                Method::get,
+                ProtocolVersion::v11,
+            ));
+            $this->fail('it should throw');
+        } catch (\Exception $e) {
+            $this->assertSame('foo', $e->getMessage());
+        }
+
+        $response = $app->run(new ServerRequest(
+            Url::of('/_profiler/'),
+            Method::get,
+            ProtocolVersion::v11,
+        ));
+
+        $this->assertSame(StatusCode::ok, $response->statusCode());
+        $profile = Reader::default()($response->body())
+            ->flatMap(Element::body())
+            ->flatMap(Element::of('main'))
+            ->flatMap(Element::of('li'))
+            ->flatMap(Element::of('a'))
+            ->match(
+                static fn($a) => $a->href(),
+                static fn() => null,
+            );
+        $this->assertNotNull($profile);
+
+        $response = $app->run(new ServerRequest(
+            $profile,
+            Method::get,
+            ProtocolVersion::v11,
+        ));
+
+        $this->assertSame(StatusCode::ok, $response->statusCode());
+        $sections = Reader::default()($response->body())
+            ->flatMap(Element::body())
+            ->flatMap(Element::of('header'))
+            ->map(static fn($header) => Elements::of('li')($header))
+            ->map(
+                static fn($lis) => $lis
+                    ->flatMap(Elements::of('a'))
+                    ->map(static fn($a) => $a->content())
+                    ->map(Str::of(...))
+                    ->map(static fn($text) => $text->trim()->toString()),
+            )
+            ->match(
+                static fn($sections) => $sections->toList(),
+                static fn() => [],
+            );
+
+        $this->assertSame(
+            ['Exception', 'Http', 'Environment', 'App graph'],
+            $sections,
+        );
+    }
+
+    public function testRecordHttpResponse()
+    {
+        $app = Application::http(Factory::build(), Environment::test([['PATH', \getenv('PATH')]]))
+            ->map(Debug::inApp()->operatingSystem())
+            ->map(Profiler::inApp($this->storage))
+            ->map(Debug::inApp()->app())
+            ->appendRoutes(static fn($routes, $_, $os) => $routes->add(
+                Route::literal('GET /')->handle(static function($request) use ($os) {
+                    return new Response(
+                        StatusCode::ok,
+                        $request->protocolVersion(),
+                    );
+                }),
+            ));
+
+        $response = $app->run(new ServerRequest(
+            Url::of('/'),
+            Method::get,
+            ProtocolVersion::v11,
+        ));
+
+        $this->assertSame(StatusCode::ok, $response->statusCode());
+
+        $response = $app->run(new ServerRequest(
+            Url::of('/_profiler/'),
+            Method::get,
+            ProtocolVersion::v11,
+        ));
+
+        $this->assertSame(StatusCode::ok, $response->statusCode());
+        $profile = Reader::default()($response->body())
+            ->flatMap(Element::body())
+            ->flatMap(Element::of('main'))
+            ->flatMap(Element::of('li'))
+            ->flatMap(Element::of('a'))
+            ->match(
+                static fn($a) => $a->href(),
+                static fn() => null,
+            );
+        $this->assertNotNull($profile);
+
+        $response = $app->run(new ServerRequest(
+            $profile,
+            Method::get,
+            ProtocolVersion::v11,
+        ));
+
+        $this->assertSame(StatusCode::ok, $response->statusCode());
+        $sections = Reader::default()($response->body())
+            ->flatMap(Element::body())
+            ->flatMap(Element::of('header'))
+            ->map(static fn($header) => Elements::of('li')($header))
+            ->map(
+                static fn($lis) => $lis
+                    ->flatMap(Elements::of('a'))
+                    ->map(static fn($a) => $a->content())
+                    ->map(Str::of(...))
+                    ->map(static fn($text) => $text->trim()->toString()),
+            )
+            ->match(
+                static fn($sections) => $sections->toList(),
+                static fn() => [],
+            );
+
+        $this->assertSame(
+            ['Http', 'Environment', 'App graph'],
+            $sections,
+        );
+    }
+
     public function testRecordCliContext()
     {
         $app = Application::cli(Factory::build(), Environment::test([['PATH', \getenv('PATH')]]))
+            ->map(Profiler::inApp($this->storage))
+            ->map(Debug::inApp())
+            ->command(static fn($_, $os) => new class($os) implements CliCommand {
+                public function __construct(private $os)
+                {
+                }
+
+                public function __invoke(Console $console): Console
+                {
+                    $this
+                        ->os
+                        ->control()
+                        ->processes()
+                        ->execute(Command::foreground('cat hello'))
+                        ->wait()
+                        ->match(
+                            static fn() => null,
+                            static fn() => null,
+                        );
+                    $this
+                        ->os
+                        ->remote()
+                        ->http()(new Request(
+                            Url::of('https://github.com'),
+                            Method::get,
+                            ProtocolVersion::v11,
+                        ))
+                        ->match(
+                            static fn() => null,
+                            static fn() => null,
+                        );
+
+                    throw new \Exception('foo');
+                }
+
+                public function usage(): string
+                {
+                    return 'hello-world';
+                }
+            });
+
+        try {
+            $app->run(InMemory::of(
+                [],
+                true,
+                ['bin', 'hello-world'],
+                [['PATH', \getenv('PATH')]],
+                '/',
+            ));
+            $this->fail('it should throw');
+        } catch (\Exception $e) {
+            $this->assertSame('foo', $e->getMessage());
+        }
+
+        $app = Application::http(Factory::build(), Environment::test([['PATH', \getenv('PATH')]]))
             ->map(Debug::inApp()->operatingSystem())
+            ->map(Profiler::inApp($this->storage))
+            ->map(Debug::inApp()->app());
+
+        $response = $app->run(new ServerRequest(
+            Url::of('/_profiler/'),
+            Method::get,
+            ProtocolVersion::v11,
+        ));
+
+        $this->assertSame(StatusCode::ok, $response->statusCode());
+        $profile = Reader::default()($response->body())
+            ->flatMap(Element::body())
+            ->flatMap(Element::of('main'))
+            ->flatMap(Element::of('li'))
+            ->flatMap(Element::of('a'))
+            ->match(
+                static fn($a) => $a->href(),
+                static fn() => null,
+            );
+        $this->assertNotNull($profile);
+
+        $response = $app->run(new ServerRequest(
+            $profile,
+            Method::get,
+            ProtocolVersion::v11,
+        ));
+
+        $this->assertSame(StatusCode::ok, $response->statusCode());
+        $sections = Reader::default()($response->body())
+            ->flatMap(Element::body())
+            ->flatMap(Element::of('header'))
+            ->map(static fn($header) => Elements::of('li')($header))
+            ->map(
+                static fn($lis) => $lis
+                    ->flatMap(Elements::of('a'))
+                    ->map(static fn($a) => $a->content())
+                    ->map(Str::of(...))
+                    ->map(static fn($text) => $text->trim()->toString()),
+            )
+            ->match(
+                static fn($sections) => $sections->toList(),
+                static fn() => [],
+            );
+
+        $this->assertSame(
+            ['Exception', 'Environment', 'Processes', 'Remote/Http', 'App graph'],
+            $sections,
+        );
+    }
+
+    public function testRecordCliContextWithoutOSLayer()
+    {
+        $app = Application::cli(Factory::build(), Environment::test([['PATH', \getenv('PATH')]]))
             ->map(Profiler::inApp($this->storage))
             ->map(Debug::inApp()->app())
             ->command(static fn($_, $os) => new class($os) implements CliCommand {
@@ -256,7 +524,93 @@ class FunctionalTest extends TestCase
             );
 
         $this->assertSame(
-            ['Exception', 'Environment', 'Processes', 'Remote/Http', 'App graph'],
+            ['Exception', 'Environment', 'App graph'],
+            $sections,
+        );
+    }
+
+    public function testRecordCliReturnCode()
+    {
+        $app = Application::cli(Factory::build(), Environment::test([['PATH', \getenv('PATH')]]))
+            ->map(Profiler::inApp($this->storage))
+            ->map(Debug::inApp()->app())
+            ->command(static fn($_, $os) => new class($os) implements CliCommand {
+                public function __construct(private $os)
+                {
+                }
+
+                public function __invoke(Console $console): Console
+                {
+                    return $console->exit(1);
+                }
+
+                public function usage(): string
+                {
+                    return 'hello-world';
+                }
+            });
+
+        $environment = $app->run(InMemory::of(
+            [],
+            true,
+            ['bin', 'hello-world'],
+            [['PATH', \getenv('PATH')]],
+            '/',
+        ));
+
+        $this->assertSame(1, $environment->exitCode()->match(
+            static fn($exit) => $exit->toInt(),
+            static fn() => null,
+        ));
+
+        $app = Application::http(Factory::build(), Environment::test([['PATH', \getenv('PATH')]]))
+            ->map(Debug::inApp()->operatingSystem())
+            ->map(Profiler::inApp($this->storage))
+            ->map(Debug::inApp()->app());
+
+        $response = $app->run(new ServerRequest(
+            Url::of('/_profiler/'),
+            Method::get,
+            ProtocolVersion::v11,
+        ));
+
+        $this->assertSame(StatusCode::ok, $response->statusCode());
+        $profile = Reader::default()($response->body())
+            ->flatMap(Element::body())
+            ->flatMap(Element::of('main'))
+            ->flatMap(Element::of('li'))
+            ->flatMap(Element::of('a'))
+            ->match(
+                static fn($a) => $a->href(),
+                static fn() => null,
+            );
+        $this->assertNotNull($profile);
+
+        $response = $app->run(new ServerRequest(
+            $profile,
+            Method::get,
+            ProtocolVersion::v11,
+        ));
+
+        $this->assertSame(StatusCode::ok, $response->statusCode());
+        $sections = Reader::default()($response->body())
+            ->flatMap(Element::body())
+            ->flatMap(Element::of('header'))
+            ->map(static fn($header) => Elements::of('li')($header))
+            ->map(
+                static fn($lis) => $lis
+                    ->flatMap(Elements::of('a'))
+                    ->map(static fn($a) => $a->content())
+                    ->map(Str::of(...))
+                    ->map(static fn($text) => $text->trim()->toString()),
+            )
+            ->match(
+                static fn($sections) => $sections->toList(),
+                static fn() => [],
+            );
+
+        $this->assertSame(
+            ['Environment', 'App graph'],
             $sections,
         );
     }
